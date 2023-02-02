5c04b54bce21ffea02187cf45e48d78ffcbd2fd6
Olga Brazhkina <emelyanova84@yandex.ru>
2023-02-02 12:14:54 +0500

    Add tests testNG

--- src/test/java/org/example/CashbackHackServiceTest.java
+++ src/test/java/org/example/CashbackHackServiceTest.java
@@ -1,31 +1,32 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+
+
+public class CashbackHackServiceTest {
 
     @Test
-    void shouldDisplayRemainIfNormalAmount() {
+    public void shouldDisplayRemainIfNormalAmount() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(900);
         int expected = 100;
 
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
     @Test
-    void shouldDisplayRemainIfMaxBoundaryAmount() {
+    public void shouldDisplayRemainIfMaxBoundaryAmount() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
         int expected = 0;
 
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, actual);
 
     }
 }
